1bc416eaf42bba68e80d59ebbafe4fa335079e31
marketing353 <marketing@ridegroup.com>
2025-12-04 13:41:37 -0500

    Remove CDN import map to avoid React duplication

--- api/checkout.js
+++ api/checkout.js
@@ -1,4 +1,6 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 /**
  * SERVERLESS FUNCTION: Stripe Payment Intent
